bf577c84a49c46a9d97765c627c177eabe64b9fc
Ramzan <ramzankhan7871@gmail.com>
2025-12-08 23:55:09 +0000

    new code

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return 'We are running our first container - from CoderCo'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
